8ab9cba3d64fa7c3ccdf95cd0828c22352ecd159
Tymoteusz Luczko <tymoteusz.luczko@gmail.com>
2021-08-10 17:40:14 +0200

    car_evaluation - need plvl sns

---
+++ small_projects/car.py
@@ -0,0 +1,205 @@
+# Python Project Template
+
+# 1. Prepare Problem
+# a) Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+# b) Load dataset
+names = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+dataset = pd.read_csv('car.data', names=names)
+
+lb=LabelEncoder()
+df = dataset.copy()
+
+for i in df.columns:
+    df[i]=lb.fit_transform(df[i])
+
+print(df.head())
+
+
+# 2. Summarize Data
+# a) Descriptive statistics
+''' print(dataset.shape)
+pd.set_option('display.max_rows', 500)
+print(dataset.dtypes)
+pd.set_option('display.width', 100)
+print(dataset.head(20))
+np.printoptions(precision=3)
+print(dataset.describe())
+print(dataset.groupby('class').size()) '''
+
+print(df.shape)
+pd.set_option('display.max_rows', 500)
+print(df.dtypes)
+pd.set_option('display.width', 100)
+print(df.head(20))
+np.printoptions(precision=3)
+print(df.describe())
+print(df.groupby('class').size())
+#clear data, no missing values
+#types object
+#1728/7
+#uneven class
+
+# b) Data visualizations
+''' dataset['class'].value_counts().plot(kind='bar', figsize=(8,8))
+plt.show() '''
+''' sns.countplot(dataset['class'])
+plt.show()
+ '''
+sns.heatmap(df.corr(), annot=True)
+plt.show()
+
+# 3. Prepare Data
+# a) Data Cleaning
+# b) Feature Selection
+# c) Data Transforms
+
+# 4. Evaluate Algorithms
+# a) Split-out validation dataset
+array = df.values
+X = array[:,0:6]
+Y = array[:,6]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,
+test_size=validation_size, random_state=seed)
+# b) Test options and evaluation metric
+num_folds = 10
+seed = 7
+scoring = 'accuracy'
+# c) Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=None)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = plt.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+#cart wypierdala w kosmos accuracy
+
+ # Standardize the dataset
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR',
+LogisticRegression())])))
+pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()),('LDA',
+LinearDiscriminantAnalysis())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN',
+KNeighborsClassifier())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART',
+DecisionTreeClassifier())])))
+pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()),('NB',
+GaussianNB())])))
+pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()),('SVM', SVC())])))
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds, random_state=None)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+fig = plt.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+# 5. Improve Accuracy
+# a) Algorithm Tuning
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+params = {'max_leaf_nodes': list(range(2, 100)), 'min_samples_split': [2, 3, 4]}
+model = DecisionTreeClassifier()
+kfold = KFold(n_splits=num_folds, random_state=None)
+grid = GridSearchCV(estimator=model, param_grid=params, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# b) Ensembles
+ensembles = []
+ensembles.append(('AB', AdaBoostClassifier()))
+ensembles.append(('GBM', GradientBoostingClassifier()))
+ensembles.append(('RF', RandomForestClassifier()))
+ensembles.append(('ET', ExtraTreesClassifier()))
+results = []
+names = []
+for name, model in ensembles:
+    kfold = KFold(n_splits=num_folds, random_state=None)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Ensemble Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+# 6. Finalize Model
+# a) Predictions on validation dataset
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = DecisionTreeClassifier(max_leaf_nodes=90, min_samples_split=2)
+model.fit(rescaledX, Y_train)
+# estimate accuracy on validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+# b) Create standalone model on entire training dataset
+# c) Save model for later use
